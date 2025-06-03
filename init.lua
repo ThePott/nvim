e573bc749b980228a59c3d39f4598ec167415790
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-06-03 12:13:32 +0900

    lualine setup done

--- init.lua
+++ init.lua
@@ -1,15 +1,10 @@
 require("config.keymap")
 require("config.lazy")
 --require("config.colorscheme")
--- require("config.lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
 
--- -- Enable autosave
--- vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
---   pattern = "*",
---   command = "silent! write",
--- })
+vim.cmd("set background=dark")
+vim.cmd("let g:everforest_background = 'hard'")
+vim.cmd("colorscheme everforest")
+-- vim.cmd("colorscheme tokyonight")
 
--- vim.opt.termguicolors = true
--- vim.cmd("colorscheme everforest")
-vim.cmd("colorscheme tokyonight")
--- vim.cmd("colorscheme nightfox")
+
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,15 @@ return {
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 
+      'c', 'comment', 'css', 
+      'diff', 
+      'html', 
+      'javascript',
+      'lua', 'luadoc', 
+      'markdown', 'markdown_inline', 
+      'typescript',
+      'query', 'vim', 'vimdoc' },
       auto_install = true,
       highlight = {
         enable = true,
