a63055d2f3deb2d4bf9863ced9f7fd2b82ab3f5b
Tynahan <nusilite@gmail.com>
2025-06-17 19:21:22 +0900

    lua typescript done

--- lsp/vtsls.lua
+++
@@ -1,9 +0,0 @@
-return {
-    cmd = {"vtsls"},
-    -- filetypes = {"js", "ts", "jsx", "tsx"},
-    filetypes = {"ts", "typescript", },
-    root_markers = {".git"},
-
-    single_file_support = true,
-    log_level = vim.lsp.protocol.MessageType.Warning
-}
