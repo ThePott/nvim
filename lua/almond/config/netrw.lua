84870c87cb0bb637669bf98c37709f5c125c3eaf
Tynahan <nusilite@gmail.com>
2025-06-24 20:54:37 +0900

    ⚰️ Chore(comment): remove unnecessary comment

--- lua/almond/config/netrw.lua
+++ lua/almond/config/netrw.lua
@@ -4,4 +4,3 @@ g.netrw_list_hide = [[^\./$,^\.\./$]]
 g.netrw_sort_sequence = '*,\\/$'
 g.netrw_banner = 0
 g.netrw_bufsettings = "noma momod nu nobl nowrap ro"
- -- vim.cmd([[let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro']])
