cf7efa4e6342cda14bec53e69d435084477a1879
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-11-01 11:22:30 +0900

    feat: setup custom plugin in lab folder

--- lua/almond/plugins/dev.lua
+++ lua/almond/plugins/dev.lua
@@ -1,10 +1,6 @@
--- print("---- dev lua is loaded")
 return {
 	{
-		dir = "~/custom-nvim-plugins/scrolloff-extreme.nvim",
-		-- dir = vim.fn.expand("~/custom-nvim-plugins/scrolloff-extreme.nvim"),
-		-- dir = "/Users/haheungju/custom-nvim-plugins/scrolloff-extreme.nvim",
-		-- name = "scrolloff-extreme", -- Add this
+		dir = "~/.config/nvim/laboratory/lua-basic.nvim/",
 		lazy = false, -- Force immediate loading
 	},
 }
