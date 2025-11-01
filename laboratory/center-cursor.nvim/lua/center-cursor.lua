47a6f291c97cba56b1d3f58ed198fc3fbd930ed5
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-11-01 13:19:20 +0900

    feat: print current row number

--- laboratory/center-cursor.nvim/lua/center-cursor.lua
+++ laboratory/center-cursor.nvim/lua/center-cursor.lua
@@ -1,14 +1,7 @@
 local M = {}
-local scrolloff = vim.o.scrolloff
-
--- print("---- yeah loaded for sure")
 
 local printOnTrigger = function()
-	print("----re-locate-cursor")
-	local win_height = vim.fn.winheight(0)
-
-	vim.o.scrolloff = win_height % 2 == 0 and scrolloff - 1 or scrolloff
-	print("----current offset", vim.o.scrolloff)
+	print("----", "scrolloff", vim.o.scrolloff, "win cur line", vim.fn.winline())
 end
 
 M.printOnTrigger = printOnTrigger
