22aca6706458f223eafa680bccb121ff3678498b
Tynahan <nusilite@gmail.com>
2026-02-20 00:09:54 +0900

    feat: set background of selection black

--- init.lua
+++ init.lua
@@ -3,4 +3,4 @@ require("almond.config")
 -- vim.cmd("colorscheme gruvbox")
 vim.cmd("colorscheme ghostty-default-style-dark")
 
--- vim.cmd("hi Visual guibg=#000000")
+vim.cmd("hi Visual guibg=#000000")
