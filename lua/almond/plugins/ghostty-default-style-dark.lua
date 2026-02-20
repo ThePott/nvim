b066d7974c177497aa0a7b160e3886b3fa55c869
Tynahan <nusilite@gmail.com>
2026-02-20 10:48:14 +0900

    fix: fix unused var color more bright

--- lua/almond/plugins/ghostty-default-style-dark.lua
+++ lua/almond/plugins/ghostty-default-style-dark.lua
@@ -2,8 +2,14 @@ return {
 	"nkxxll/ghostty-default-style-dark.nvim",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		require("ghostty-default-style-dark").setup({})
+	opts = {
+		on_highlights = function(highlights, colors)
+			highlights.DiagnosticUnnecessary = { fg = "#888888" }
+			highlights.Visual = { bg = "#000000" }
+		end,
+	},
+	config = function(_, opts)
+		require("ghostty-default-style-dark").setup(opts)
 		vim.cmd.colorscheme("ghostty-default-style-dark")
 	end,
 }
