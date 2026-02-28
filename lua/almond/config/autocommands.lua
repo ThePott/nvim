a912eda5d2011444afd880aa03d60091858f7157
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-02-28 11:06:35 +0900

    temp

--- lua/almond/config/autocommands.lua
+++ lua/almond/config/autocommands.lua
@@ -1,6 +1,14 @@
 local nvim_create_autocmd = vim.api.nvim_create_autocmd
+
 nvim_create_autocmd("InsertLeave", {
 	callback = function()
 		vim.fn.system("im-select com.apple.keylayout.ABC")
+	end,
+})
+
+nvim_create_autocmd("FileType", {
+	pattern = "help",
+	callback = function()
+		vim.cmd("only")
 	end,
 })
