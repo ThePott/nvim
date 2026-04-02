f8d9b034bd14a11bf386b489ba384d7f28819137
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-04-02 22:37:01 +0900

    feat: add ensure installed list in mason tool installer

--- lua/almond/plugins/mason-tool-installer.lua
+++ lua/almond/plugins/mason-tool-installer.lua
@@ -1,17 +1,35 @@
 return {
 	"WhoIsSethDaniel/mason-tool-installer.nvim",
 	dependencies = {
+		"neovim/nvim-lspconfig",
 		{ "mason-org/mason.nvim", opts = {} },
 		{ "mason-org/mason-lspconfig.nvim", opts = {} },
 		{ "jay-babu/mason-nvim-dap.nvim", opts = {} },
 	},
 	opts = {
 		ensure_installed = {
+			-- other than lsp
 			"prettier",
 			"stylua",
 			"eslint_d",
 			"typstyle",
 			"codelldb",
+			"oxfmt",
+			"oxlint",
+
+			-- lsp
+			"lua-language-server",
+			"vtsls",
+			"tailwindcss-language-server",
+			"pyright",
+			"json-lsp",
+			"html-lsp",
+			"emmet-ls",
+			"prisma-language-server",
+			"zls",
+			"vim-language-server",
+			"tinymist",
+			"clangd",
 		},
 	},
 }
