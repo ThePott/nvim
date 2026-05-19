bcc2b1c13c040aede36cfa2ed36f29b46098f6b3
Tynahan <nusilite@gmail.com>
2026-05-19 16:33:57 +0900

    fix: place color override before colorscheme command

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 vim.pack.add({
     "https://github.com/nkxxll/ghostty-default-style-dark.nvim",
 })
-vim.cmd("colorscheme ghostty-default-style-dark")
-
-local require_all = require("almond.utils.require-all")
-require_all.require_all("almond.config")
-require_all.require_all("almond.plugins")
-require_all.require_all("almond.plugins.test")
-
 require("ghostty-default-style-dark").setup({
+    -- NOTE: must be called before colorscheme command
     on_highlights = function(highlights, colors)
         print("working")
         highlights.DiagnosticUnnecessary = { fg = "#888888" }
         highlights.Visual = { bg = "#000000" }
     end,
 })
+vim.cmd("colorscheme ghostty-default-style-dark")
+
+local require_all = require("almond.utils.require-all")
+require_all.require_all("almond.config")
+require_all.require_all("almond.plugins")
+require_all.require_all("almond.plugins.test")
