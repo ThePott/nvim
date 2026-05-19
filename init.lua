b14a886de9b1b3cd9c192ece3bcd31ee33c3349f
Tynahan <nusilite@gmail.com>
2026-05-19 14:43:17 +0900

    fix: fix require-all import base path

--- init.lua
+++ init.lua
@@ -4,6 +4,6 @@ vim.pack.add({
 vim.cmd("colorscheme ghostty-default-style-dark")
 
 local require_all = require("almond.utils.require-all")
-require_all.require_all("lua.almond.config")
-require_all.require_all("lua.almond.plugins")
-require_all.require_all("lua.almond.plugins.test")
+require_all.require_all("almond.config")
+require_all.require_all("almond.plugins")
+require_all.require_all("almond.plugins.test")
--- lua/almond/utils/require-all.lua
+++ lua/almond/utils/require-all.lua
@@ -1,8 +1,7 @@
 local Module = {}
-function Module.require_all(module_path)
-    -- module_path is the Lua module path like "lua.utils" or "utils"
-    local cwd = vim.fn.getcwd()
-    local dir = cwd .. "/" .. module_path:gsub("%.", "/")
+function Module.require_all(module_path) -- NOTE: after `lua/`
+    -- module_path is the Lua module path like "almond.config" or "almond"
+    local dir = "~/.config/nvim/lua/" .. module_path:gsub("%.", "/")
 
     local files = vim.fn.glob(dir .. "/*.lua", false, true)
     for _, file in ipairs(files) do
